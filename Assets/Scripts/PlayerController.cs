fd51442147fec6f07eb15e0ca3939b1c645dfff2
TheFenixOfficial <96136517+TheFenixOfficial@users.noreply.github.com>
2024-01-26 19:58:23 +0100

    GroundCheck

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,9 +8,12 @@ public class PlayerController : MonoBehaviour
     private float speed = 12f;
     private CharacterController controller;
 
+    public Transform groundCheck;
+    public LayerMask groundMask;
+
     private void Start()
     {
-      controller = GetComponent<CharacterController>();   
+        controller = GetComponent<CharacterController>();
     }
 
     private void Update()
@@ -25,5 +28,27 @@ public class PlayerController : MonoBehaviour
 
         Vector3 move = transform.right * x + transform.forward * z;
         controller.Move(move * speed * Time.deltaTime);
+    }
+
+    private void CheckGround()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(groundCheck.position, transform.TransformDirection(Vector3.down), out hit, 0.4f, groundMask))
+        {
+            string terrainType = hit.collider.gameObject.tag;
+
+            switch (terrainType)
+            {
+                default:
+                    speed = 12f;
+                    break;
+                case "Low":
+                    speed = 3f;
+                    break;
+                case "High":
+                    speed = 20f;
+                    break;
+            }
+        }
     }
 }
