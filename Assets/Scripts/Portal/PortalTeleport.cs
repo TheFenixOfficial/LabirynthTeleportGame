7ae5de36d5bf525539d02329c451992ece661910
TheFenixOfficial <96136517+TheFenixOfficial@users.noreply.github.com>
2024-02-16 20:06:08 +0100

    Portal

---
+++ Assets/Scripts/Portal/PortalTeleport.cs
@@ -0,0 +1,53 @@
+using UnityEngine;
+
+public class PortalTeleport : MonoBehaviour
+{
+    public Transform player;
+    public Transform receiver;
+
+    private bool playerIsOverlapping = false;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            playerIsOverlapping = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            playerIsOverlapping = false;
+        }
+    }
+
+    private void Teleportation()
+    {
+        if (playerIsOverlapping)
+        {
+            Vector3 portalToPlayer = player.position - transform.position;
+            float dotProduct = Vector3.Dot(transform.up, portalToPlayer);
+
+            if (dotProduct < 0f)
+            {
+                float rotationDiff = -Quaternion.Angle(transform.rotation, receiver.rotation);
+                rotationDiff += 180;
+
+                player.Rotate(Vector3.up, rotationDiff);
+
+                Vector3 positionOffset = Quaternion.Euler(0f, rotationDiff, 0f) * portalToPlayer;
+
+                player.position = receiver.position + positionOffset;
+
+                playerIsOverlapping = false;
+            }
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        Teleportation();
+    }
+}
